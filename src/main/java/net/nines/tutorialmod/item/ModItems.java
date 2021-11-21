44a3b8bb5201778249246718e8690c9b39f9bea5
Ninesss <623840239@qq.com>
2021-11-21 23:20:04 +0800

    add food item

--- src/main/java/net/nines/tutorialmod/item/ModItems.java
+++ src/main/java/net/nines/tutorialmod/item/ModItems.java
@@ -1,8 +1,8 @@
 package net.nines.tutorialmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.nines.tutorialmod.TutorialMod;
@@ -15,6 +15,11 @@ public class ModItems {
 
     public static final Item RUBY = registerItem("ruby",
             new Item(new FabricItemSettings().group(ModItemGroup.RUBY)));
+
+    public static final Item PEPPER = registerItem("pepper",
+            new Item(new FabricItemSettings()
+                    .food(new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build())
+                    .group(ModItemGroup.RUBY)));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
